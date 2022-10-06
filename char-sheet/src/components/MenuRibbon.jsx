a6c57e572354e7d595c0320f5aba2b9c8bca14c3
Tim Lamborn <tlamborn@gmail.com>
2022-10-06 18:00:08 +1300

    Add in edit button

--- char-sheet/src/components/MenuRibbon.jsx
+++ char-sheet/src/components/MenuRibbon.jsx
@@ -54,7 +54,14 @@ function MenuRibbon( {editable, setEditable, sheet, setSheet, children} ) {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
           </label>
         </div>
-        <div className="flex-1 px-2 mx-2">Navbar Title</div>
+        <div className="flex-1 px-2 mx-2">
+        <IconButton
+          icon={editable ? 'check' : 'edit'}
+          btnStyle={editable ? 'btn-primary' : 'btn-ghost'}
+          btnSize='btn-md'
+          onClick={() => setEditable(!editable)}
+        />
+        </div>
         <div className="flex-none hidden lg:block">
           <ul className="menu menu-horizontal">
             { menuItems }
