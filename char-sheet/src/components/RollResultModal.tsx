92834de4e6e2ffc1a4dbbc995bdf4cf3fd9ab91c
Tim Lamborn <tlamborn@gmail.com>
2022-10-29 16:11:41 +1300

    Animate rolls

--- char-sheet/src/components/RollResultModal.tsx
+++ char-sheet/src/components/RollResultModal.tsx
@@ -20,19 +20,24 @@ function RollResultModal({results, close}: {
     <h1 className='font-bold text-2xl mb-4'>Roll result</h1>
     <div className='flex gap-4 flex-row flex-wrap justify-center'>
       {
-        
-      results.map( r => {
+      results.map( (r, i) => {
         let success = r >= 4
         return <div
         ><svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-12 w-12"
+          style={{
+            animation: "ping 0.5s cubic-bezier(.49,.01,.83,.45)",
+            animationDirection: "reverse",
+            animationDelay: `${0.1 * i}s`,
+            animationFillMode: "backwards"
+          }}
           viewBox="0 0 100 100"
           strokeWidth={0}
           preserveAspectRatio="xMidYMid meet"
           >
             <polygon points="0,86 100,86 50,0"
-              style={{strokeWidth: 0}}
+              style={{strokeWidth: 2, stroke: success ? "hsl(var(--su))" : "hsl(var(--er))"}}
               fill={success ? "hsl(var(--suc))" : "hsl(var(--erc))"}
             />
             <text
