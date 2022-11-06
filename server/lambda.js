7c8d2b46adddb1f83b1ace9274f5754703b4e2fc
Tim Lamborn <tlamborn@gmail.com>
2022-11-06 19:51:50 +1300

    Start on caltrops server component

---
+++ server/lambda.js
@@ -0,0 +1,113 @@
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'caltrops-sheets';
+
+async function writeContent(uid, title, user, content) {
+    const item = {
+        "id": uid,
+        "owner": user,
+        "title": title,
+        "content": content,
+        "time": (new Date()).toISOString()
+    };
+    await db.put({
+        TableName: TABLE_NAME,
+        Item: item
+    }).promise();
+}
+
+async function listContent(user) {
+    return (await db.query({
+        TableName: TABLE_NAME,
+        IndexName: "owner-index",
+        KeyConditionExpression: "#o = :o",
+        ExpressionAttributeValues: {
+            ":o": user
+        },
+        ExpressionAttributeNames: {
+            "#o": "owner"
+        },
+        Select: "ALL_PROJECTED_ATTRIBUTES"
+    }).promise()).Items
+}
+
+async function readContent(uid) {
+    const response = await db.get({
+        TableName: TABLE_NAME,
+        Key: {
+            id: uid,
+        }
+    }).promise()
+    return response.Item ?? null
+}
+
+function errorResponse(status, source, exception = undefined) {
+    return {
+        statusCode: status,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            "error": exception === undefined ? source : `${source}: ${exception.toString}`
+        },
+    };
+}
+
+function authenticate(user) {
+    return !!user;
+}
+
+exports.handler = async (event) => {
+
+    let body = null;
+    try {
+        let body = JSON.parse(event.body);
+    } catch (error) {
+        return errorResponse(400, "Error parsing body", error);
+    }
+
+    let reply = {};
+
+    if (!authenticate(body.user)) {
+        return errorResponse(401, "Unauthorised");
+    }
+
+    if (body.write) {
+        try {
+            for (const info of body.write) {
+                await writeContent(info.id, info.title, body.user, info.content);
+            }
+        } catch (error) {
+            return errorResponse(500, "Error writing content", error);
+        }
+    }
+
+    if (body.read) {
+        try {
+            let read = []
+            for (const uid of body.read) {
+                read.push(await readContent(uid));
+            }
+            reply.read = read;
+        } catch (error) {
+            return errorResponse(500, "Error reading content", error);
+        }
+    }
+
+    if (body.list) {
+        try {
+            reply.list = await listContent(body.user);
+        } catch (error) {
+            return errorResponse(500, "Error listing content", error);
+        }
+    }
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: reply,
+    };
+    return response;
+};
