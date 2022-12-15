04be27cf797f5696584f8054a2e127be11993a73
Tim Lamborn <tlamborn@gmail.com>
2022-12-15 23:06:56 +1300

    Trim signature on b64 tokens

--- server/src/signature.js
+++ server/src/signature.js
@@ -1,11 +1,15 @@
 const crypto = require('crypto')
 
+function trim_b64(text) {
+    text.replace(/=*$/, '')
+}
+
 function sign(text, secret) {
-    return crypto.createHmac('sha256', secret).update(text).digest('base64')
+    return trim_b64(crypto.createHmac('sha256', secret).update(text).digest('base64'))
 }
 
 function encode(payload, secret) {
-    const text = btoa(JSON.stringify(payload));
+    const text = trim_b64(btoa(JSON.stringify(payload)));
     const signature = sign(text, secret);
     return `${text}.${signature}`;
 }
@@ -13,7 +17,7 @@ function encode(payload, secret) {
 function decode(token, secret) {
     const [text, signature] = token.split('.');
     const expected = sign(text, secret);
-    if (signature === expected) {
+    if (trim_b64(signature) === expected) {
         return JSON.parse(atob(text));
     }
     return null;
