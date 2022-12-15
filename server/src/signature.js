fc4ecf96bb5645b21c6772f56af5f3041d8541d0
Tim Lamborn <tlamborn@gmail.com>
2022-12-15 23:26:06 +1300

    Fix signature bugs
    closes #60

--- server/src/signature.js
+++ server/src/signature.js
@@ -1,15 +1,15 @@
 const crypto = require('crypto')
 
-function trim_b64(text) {
-    text.replace(/=*$/, '')
+function trimB64(text) {
+    return text.replace(/=*$/, '')
 }
 
 function sign(text, secret) {
-    return trim_b64(crypto.createHmac('sha256', secret).update(text).digest('base64'))
+    return trimB64(crypto.createHmac('sha256', secret).update(text).digest('base64'))
 }
 
 function encode(payload, secret) {
-    const text = trim_b64(btoa(JSON.stringify(payload)));
+    const text = trimB64(btoa(JSON.stringify(payload)));
     const signature = sign(text, secret);
     return `${text}.${signature}`;
 }
@@ -17,7 +17,7 @@ function encode(payload, secret) {
 function decode(token, secret) {
     const [text, signature] = token.split('.');
     const expected = sign(text, secret);
-    if (trim_b64(signature) === expected) {
+    if (trimB64(signature) === expected) {
         return JSON.parse(atob(text));
     }
     return null;
