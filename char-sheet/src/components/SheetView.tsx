93c57beecead0e27b338a31a31776f1b49b308e1
Tim Lamborn <tlamborn@gmail.com>
2023-06-30 00:45:04 +1200

    Fix maxCostTotal name

--- char-sheet/src/components/SheetView.tsx
+++ char-sheet/src/components/SheetView.tsx
@@ -63,10 +63,10 @@ function SheetView( { rules, sheet, setSheet, editable=EditMode.Live }: {
             skills={rules.skills}
             scores={sheet.skills}
             setScores={scores => {setSheet(modifyObject(sheet, 'skills', scores))}}
+            maxCostTotal={caltrops.skillCostMax(rules, sheet.info.level)}
             editable={editable}
             roll={roll}
             setRoll={setRoll}
-            maxCost={caltrops.skillCostMax(rules, sheet.info.level)}
           />
         </section>
         
